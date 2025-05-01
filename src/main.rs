260968dcc9007429d310af11bafdf48461a66079
Cristian Romero <cristianrom66@gmail.com>
2025-05-01 17:12:53 -0500

    updates: - fmt created - first mini router app created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Json, Router,
+    routing::{get, post},
+};
+// use hyper::server::Server;
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    content: String,
+}
+
+async fn health_check() -> &'static str {
+    "OK"
+}
+
+async fn echo(Json(payload): Json<Message>) -> Json<Message> {
+    Json(payload)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(health_check)).route("/echo", post(echo));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
+    println!("Listening on http://{}", addr);
+    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app).await.unwrap();
 }
